46d0e6d91826d1bb2efe4ad0739744eda1d9cf1e
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2022-02-18 16:02:51 +0300

    Добавьте файлы проекта.

---
+++ Prakt1Var25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prakt1Var25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y;
+
+            if (x < 0 || b != 0)
+            {
+                y = a * Math.Pow(x, 4) + b * Math.Pow(x, 2);
+
+            }
+            else if (x >= 0 && c != 0)
+            {
+                y = (x + a) / c; 
+            }
+            else
+            {
+                y = (15 * x) / (c + 9); 
+            }
+
+            Console.WriteLine(y);
+        }
+    }
+}
